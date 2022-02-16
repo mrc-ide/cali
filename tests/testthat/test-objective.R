c028902c537f7ff20185e95d0c77293eb4b8e8e8
Peter Winskill <p.winskill@imperial.ac.uk>
2022-02-16 13:30:16 +0000

    Âdefine cali objective

--- tests/testthat/test-objective.R
+++ tests/testthat/test-objective.R
@@ -2,7 +2,7 @@ test_that("objective works works", {
   x <- data.frame(n_detect_730_3650 = c(0, 50, 100),
                   n_730_3650 = 100)
   p <- malariasimulation::get_parameters(list(human_population = 1000))
-  mockery::stub(objective, "malariasimulation::run_simulation", x, 2)
-  expect_equal(objective(1, p, target = 0.5, 2, summary_pfpr_2_10, 0), 0)
-  expect_equal(objective(1, p, target = 0.5, 1, summary_pfpr_2_10, 0), -0.5)
+  mockery::stub(cali::objective, "malariasimulation::run_simulation", x, 2)
+  expect_equal(cali::objective(1, p, target = 0.5, 2, summary_pfpr_2_10, 0), 0)
+  expect_equal(cali::objective(1, p, target = 0.5, 1, summary_pfpr_2_10, 0), -0.5)
 })

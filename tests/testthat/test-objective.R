68d6c9daaeb9f0dc1c0cde94f599294caa62d352
Peter Winskill <p.winskill@imperial.ac.uk>
2022-08-03 16:40:22 +0100

    Make summary and target more generic

--- tests/testthat/test-objective.R
+++ tests/testthat/test-objective.R
@@ -1,9 +1,9 @@
-test_that("objective works works", {
+test_that("objective works", {
   x <- data.frame(n_detect_730_3650 = c(0, 50, 100),
                   n_730_3650 = 100)
   p <- malariasimulation::get_parameters(list(human_population = 1000))
   mockery::stub(objective, "malariasimulation::run_simulation", x)
-  expect_equal(objective(1, p, target = 0.5, 2, summary_pfpr_2_10, 0), 0)
+  expect_equal(objective(x = 1, parameters = p, target = 0.5, summary_function = summary_mean_pfpr_2_10, tolerance = 0, weights = 1), 0)
   mockery::stub(objective, "malariasimulation::run_simulation", x)
-  expect_equal(objective(1, p, target = 0.5, 1, summary_pfpr_2_10, 0), -0.5)
+  expect_equal(objective(x = 1, parameters = p, target = 1, summary_function = summary_mean_pfpr_2_10, tolerance = 0, weights = 1), -0.5)
 })

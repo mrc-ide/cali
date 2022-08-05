3d1cfae3e2e82568bbfbcff4172a538612ad7949
Peter Winskill <p.winskill@imperial.ac.uk>
2022-08-05 09:17:38 +0100

    Improve verbose outputs

--- R/objective.R
+++ R/objective.R
@@ -8,17 +8,24 @@
 #' @return Difference between output and target.
 objective <- function(x, parameters, target, summary_function, tolerance, weights){
   message("Trying EIR: ", signif(x, 5))
+  
   p <- malariasimulation::set_equilibrium(parameters, init_EIR = x)
   raw_output <- malariasimulation::run_simulation(timesteps = p$timesteps, parameters = p)
-  target_variable <- summary_function(raw_output)
-  if(length(target_variable) != length(target)){
+  model_output <- summary_function(raw_output)
+  
+  if(length(model_output) != length(target)){
     stop("summary function must produce a vector of the same length as target")
   }
-  difference <- (target_variable - target)
-  message("Current difference: ", paste(signif(difference, 3), collapse = " "))
-  difference <- sum(difference * weights)
-  message("Current sum of absolute weighted difference: ", paste(signif(abs(difference), 3), collapse = " "))
+  
+  difference <- (model_output - target)
+  weighted_difference <- difference * weights
+  sum_weighted_difference <- sum(weighted_difference)
+  
+  print(signif(rbind(model_output, target, difference, weighted_difference)), 3)
+  message("\nCurrent sum of weighted difference: ", signif(sum_weighted_difference, 3))
+  
   # Adjust for specified tolerance
-  difference[abs(difference) < tolerance] <- 0
-  return(difference)
+  sum_weighted_difference[abs(sum_weighted_difference) < tolerance] <- 0
+  
+  return(sum_weighted_difference)
 }

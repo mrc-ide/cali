2abb94f35aff4239009fb7c42d807f994d1e8ecb
Peter Winskill <p.winskill@imperial.ac.uk>
2022-08-19 14:56:15 +0100

    Update tests

--- tests/testthat/test-objective.R
+++ tests/testthat/test-objective.R
@@ -3,7 +3,11 @@ test_that("objective works", {
                   n_730_3650 = 100)
   p <- malariasimulation::get_parameters(list(human_population = 1000))
   mockery::stub(objective, "malariasimulation::run_simulation", x)
-  expect_equal(objective(x = 1, parameters = p, target = 0.5, summary_function = summary_mean_pfpr_2_10, tolerance = 0, weights = 1), 0)
+  expect_equal(objective(x = 1, parameters = p, target = 0.5,
+                         summary_function = summary_mean_pfpr_2_10, tolerance = 0,
+                         weights = 1, elimination_penalty = NULL), 0)
   mockery::stub(objective, "malariasimulation::run_simulation", x)
-  expect_equal(objective(x = 1, parameters = p, target = 1, summary_function = summary_mean_pfpr_2_10, tolerance = 0, weights = 1), -0.5)
+  expect_equal(objective(x = 1, parameters = p, target = 1,
+                         summary_function = summary_mean_pfpr_2_10, tolerance = 0,
+                         weights = 1, elimination_penalty = NULL), -0.5)
 })

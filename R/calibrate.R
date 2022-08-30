7aaa40e674d0bb1c9af103e3e53354ef69615400
Peter Winskill <p.winskill@imperial.ac.uk>
2022-08-30 11:07:15 +0100

    Updated regime

--- R/calibrate.R
+++ R/calibrate.R
@@ -1,27 +1,24 @@
 #' Calibrate
 #' 
-#' General calibration function for malariasimulation. Please see the documentation for
-#' \link[stats]{uniroot} to become familiar with possible additional arguments and convergence
-#' errors.
+#' General calibration function for malariasimulation. 
 #'
 #' @param parameters Other malariasimulation model parameters
 #' @param target Values of target variable to calibrate to.
 #' @param summary_function A function that take the raw model output as an argument and
 #' produces a vector of the target variable.
-#' @param tolerance The routine will complete when the sum of the absolute weighted difference between the target variable 
+#' @param tolerance The routine will complete when the absolute sum of the weighted difference between the target variable 
 #' and the target values falls below this value. Tolerance is specified in 
 #' the units of the target variable (e.g. if my target variable is prevalence, and my tolerance is 0.05,
-#' then the sum of the absolute weighted difference between the target variable and the target values must be <5% for the routine to succeed).
+#' then the absolute sum of the weighted difference between the target variable and the target values must be <5% for the routine to succeed).
 #' @param weights A numerical vector of weights the same length as target giving the weights to use for elements of target.
 #' @param elimination_check Check transmission is maintained for all target points with ongoing transmission before exiting early.
 #' @param low Lower boundof EIRs
 #' @param high Upper bound of EIRs
-#' @param maxiter Maximum iterations
+#' @param maxiter Maximum iterations. If stopping criteria have not been met by `maxiter`, the current best estimate will be returned
 #'
 #' @return Uniroot output
 #' @export
 calibrate <- function(target,
-                      objective_function,
                       summary_function,
                       parameters,
                       tolerance,
@@ -38,9 +35,9 @@ calibrate <- function(target,
     y <- objective(x = x,
                    parameters = parameters,
                    summary_function = summary_function)
+    
     difference <- y - target
     weighted_difference <- difference * weights
-    
     print(signif(rbind(y, target, difference, weighted_difference)), 3)
     diff <- sum(weighted_difference)
     
--- tests/testthat/test-objective.R
+++ tests/testthat/test-objective.R
@@ -3,11 +3,7 @@ test_that("objective works", {
                   n_730_3650 = 100)
   p <- malariasimulation::get_parameters(list(human_population = 1000))
   mockery::stub(objective, "malariasimulation::run_simulation", x)
-  expect_equal(objective(x = 1, parameters = p, target = 0.5,
-                         summary_function = summary_mean_pfpr_2_10, tolerance = 0,
-                         weights = 1, elimination_penalty = NULL), 0)
+  expect_equal(objective(x = 1, parameters = p,
+                         summary_function = summary_mean_pfpr_2_10), 0.5)
   mockery::stub(objective, "malariasimulation::run_simulation", x)
-  expect_equal(objective(x = 1, parameters = p, target = 1,
-                         summary_function = summary_mean_pfpr_2_10, tolerance = 0,
-                         weights = 1, elimination_penalty = NULL), -0.5)
 })

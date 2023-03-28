71c7f8e0bfeaf591fa8fc94582416802b39098eb
Peter Winskill <p.winskill@imperial.ac.uk>
2023-03-28 15:02:54 +0100

    Revert to using uniroot

--- R/objective.R
+++ R/objective.R
@@ -6,12 +6,33 @@
 #' @inheritParams calibrate
 #'
 #' @return Difference between output and target.
-objective <- function(x, parameters, summary_function){
+objective <- function(x, parameters, summary_function, target, weights, tolerance, elimination_check){
   message("\nTrying EIR: ", signif(x, 5))
   
   p <- malariasimulation::set_equilibrium(parameters, init_EIR = x)
   raw_output <- malariasimulation::run_simulation(timesteps = p$timesteps, parameters = p)
   model_output <- summary_function(raw_output)
- 
-  return(model_output)
+  difference <- model_output - target
+  weighted_difference <- difference * weights
+  
+  print(signif(rbind(model_output, target, difference, weighted_difference)), 3)
+  
+  if(elimination_check){
+    bad_eliminated <- model_output == 0 & target != 0
+    if(sum(bad_eliminated) > 0){
+      message("Unwanted elimination")
+      weighted_difference[bad_eliminated] <- pmin(-tolerance,  weighted_difference[bad_eliminated])
+    }
+  }
+  
+  mean_weighted_difference <- mean(weighted_difference)
+  
+  message("\nSum squared weighted difference: ", signif(mean_weighted_difference, 5))
+  
+  if(mean(abs(weighted_difference)) < tolerance){
+    message("Mean absolute difference < tolerance")
+    mean_weighted_difference <- 0
+  }
+  
+  return(mean_weighted_difference)
 }
--- tests/testthat/test-objective.R
+++ tests/testthat/test-objective.R
@@ -4,6 +4,38 @@ test_that("objective works", {
   p <- malariasimulation::get_parameters(list(human_population = 1000))
   mockery::stub(objective, "malariasimulation::run_simulation", x)
   expect_equal(objective(x = 1, parameters = p,
-                         summary_function = summary_mean_pfpr_2_10), 0.5)
+                         summary_function = summary_mean_pfpr_2_10,
+                         target = 0.5,
+                         weights = 1,
+                         tolerance = 0,
+                         elimination_check = FALSE), 0)
   mockery::stub(objective, "malariasimulation::run_simulation", x)
+  expect_equal(objective(x = 1, parameters = p,
+                         summary_function = summary_mean_pfpr_2_10,
+                         target = 0,
+                         weights = 1,
+                         tolerance = 0,
+                         elimination_check = FALSE), 0.5)
+  
+  x <- data.frame(n_detect_730_3650 = c(0, 0, 0),
+                  n_730_3650 = 100)
+  mockery::stub(objective, "malariasimulation::run_simulation", x)
+  expect_equal(objective(x = 1,
+            parameters = p,
+            summary_function = summary_mean_pfpr_2_10,
+            target = 0.5,
+            weights = 1,
+            tolerance = 0.02,
+            elimination_check = TRUE), -0.5)
+  
+  x <- data.frame(n_detect_730_3650 = c(0, 0, 0),
+                  n_730_3650 = 100)
+  mockery::stub(objective, "malariasimulation::run_simulation", x)
+  expect_equal(objective(x = 1,
+                         parameters = p,
+                         summary_function = summary_mean_pfpr_2_10,
+                         target = 0.001,
+                         weights = 1,
+                         tolerance = 0.02,
+                         elimination_check = TRUE), -0.02)
 })
